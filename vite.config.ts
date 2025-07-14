b590495e936f1fcf903c55071be49777f90fc0c2
Sean Lewis <sean.alan.lewis@gmail.com>
2025-07-14 22:35:29 +0100

    fix(vite.config.ts): remove incorrect base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:
-    process.env.NODE_ENV === "production" ? "/simple-extension-analyser/" : "",
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
